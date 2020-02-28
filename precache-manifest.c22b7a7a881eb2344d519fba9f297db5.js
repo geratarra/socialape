e37927661db597988cf2d62c979e7fa353c565a9
gtarragona <gtarragona@switchfly.com>
2020-02-28 13:24:48 -0700

    Updates

---
+++ precache-manifest.c22b7a7a881eb2344d519fba9f297db5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8820ce6ee6855504813b55cafeb86c46",
+    "url": "/socialape/index.html"
+  },
+  {
+    "revision": "67295371fdd377a6b09a",
+    "url": "/socialape/static/css/main.ccd472c9.chunk.css"
+  },
+  {
+    "revision": "4f57fbf3c0b1b10f3995",
+    "url": "/socialape/static/js/2.6160a14e.chunk.js"
+  },
+  {
+    "revision": "3d416322b0ec4a5a91d5be4afe7c848c",
+    "url": "/socialape/static/js/2.6160a14e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67295371fdd377a6b09a",
+    "url": "/socialape/static/js/main.fc4f6092.chunk.js"
+  },
+  {
+    "revision": "4b90665b5df6f6a5c677",
+    "url": "/socialape/static/js/runtime-main.b3188eea.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/socialape/static/media/no-img.5a021ab9.png"
+  }
+]);
